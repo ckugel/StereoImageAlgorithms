be421be04983b27e7c835b8e292478263c1b5ba3
Caleb <calebkugel1@gmail.com>
2022-12-17 00:35:08 -0600

    cope cope cope

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+
+void stereoSearch(Mat im1, Mat im2) {
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
