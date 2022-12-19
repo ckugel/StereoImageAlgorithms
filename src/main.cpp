6b650cec77c4d818f68e5dbf636e46a4715c86e8
Caleb <calebkugel1@gmail.com>
2022-12-18 23:15:26 -0600

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 using namespace cv;
 
-
-
-void stereoSearch(Mat im1, Mat im2) {
+void stereoSearch(Mat& im1, Mat& im2) {
 
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat imgLeft = imread("/images/left.jpg", IMREAD_COLOR);
+    Mat imgRight = imread("/images/right.jpg", IMREAD_COLOR);
+
+
+
     return 0;
 }
